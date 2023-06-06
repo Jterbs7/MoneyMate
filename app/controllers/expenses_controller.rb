c9c4ddb5e3d51baebaf5f6f941ef5929624c399b
Hadi <130841358+hadi-mukhtar@users.noreply.github.com>
2023-06-06 23:20:00 +0200

    Add actions for expenses controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,2 +1,49 @@
 class ExpensesController < ApplicationController
+  before_action :set_expense, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @expenses = Expense.all
+  end
+
+  def show
+  end
+
+  def new
+    @expense = Expense.new
+  end
+
+  def create
+    @expense = Expense.new(expense_params)
+    if @expense.save
+      redirect_to @expense, notice: 'Expense was successfully added.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @expense.update(expense_params)
+      redirect_to @expense, notice: 'Expense was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @expense.destroy
+    redirect_to expenses_url, notice: 'Expense was successfully deleted.'
+  end
+
+  private
+
+  def set_expense
+    @expense = Expense.find(params[:id])
+  end
+
+  def expense_params
+    params.require(:expense).permit(:amount, :description, :merchant, :date, :budget_id)
+  end
 end
