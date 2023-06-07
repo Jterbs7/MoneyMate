51bf4a8bfbfee57c8cf30ec30e7fd9967f943ac8
justin terblanche <jterblanche7@gmail.com>
2023-06-07 13:31:34 +0200

    Created a fix for budget show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :budgets, except: [:show]
+  resources :budgets
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
