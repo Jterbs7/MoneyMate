c14b22cbcf2a17f6c0cecac24fcb390452c9f75b
Hadi <130841358+hadi-mukhtar@users.noreply.github.com>
2023-06-08 15:19:39 +0200

    Add profiles controller

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,46 @@
+class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_profile, only: [:show, :edit, :update]
+
+  def show
+  end
+
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    @profile = current_user.create_profile(profile_params)
+
+    if @profile.save
+      flash[:notice] = 'Profile was successfully created.'
+      redirect_to @profile
+    else
+      flash.now[:alert] = 'There was an error creating the profile.'
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @profile.update(profile_params)
+      flash[:notice] = 'Profile was successfully updated.'
+      redirect_to @profile
+    else
+      flash.now[:alert] = 'There was an error updating the profile.'
+      render :edit
+    end
+  end
+
+  private
+
+  def set_profile
+    @profile = current_user.profile
+  end
+
+  def profile_params
+    params.require(:profile).permit(:first_name, :second_name, :username, :age, :monthly_income)
+  end
+end
