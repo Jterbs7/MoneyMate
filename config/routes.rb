d7321d124163b86e710f8e0a7bb5372397be0958
Hadi <130841358+hadi-mukhtar@users.noreply.github.com>
2023-06-08 17:37:14 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :budgets
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :expenses
+  resources :profiles, only: [:new, :create, :show, :edit, :update] # This creates routes for profile#edit and profile#update
+  resources :category_budgets, only: [:show, :index]
+  resources :budgets, only: [:new, :create, :index, :show, :edit, :update]
+  resources :expenses, only: [:new, :create, :index, :show]
 end
