f8cf863f50cb35a7c572c86b913833aaecbf23b1
Hadi <130841358+hadi-mukhtar@users.noreply.github.com>
2023-06-09 12:41:03 +0200

    Fixed pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
-
   def home
-
-  @expenses = Expense.all
+    @expenses = Expense.all.sort_by(&:date).reverse
   end
 end
