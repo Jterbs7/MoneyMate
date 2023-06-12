c0422e6e9f1619c4a3090a5ed2559c6af2e8377b
justin terblanche <jterblanche7@gmail.com>
2023-06-12 11:52:32 +0200

    Importmap fixed

--- app/controllers/category_budgets_controller.rb
+++ app/controllers/category_budgets_controller.rb
@@ -2,6 +2,8 @@ class CategoryBudgetsController < ApplicationController
   before_action :set_category_budget, only: :show
 
   def index
+    # total_categories = CategoryBudget.sum(:amount)
+    # @data = CategoryBudget.group(:name).sum(:amount) { |v| (v / total_categories.to_f) * 100 }
     @category_budgets = CategoryBudget.all
   end
 
