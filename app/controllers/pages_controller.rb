0dcc8c532fc16c332cd905b6cf0a97aa10d8c43b
justin terblanche <jterblanche7@gmail.com>
2023-06-14 14:50:44 +0200

    new graph implemented

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,33 @@ class PagesController < ApplicationController
     total_budget = Budget.sum(:amount)
     total_expense = Expense.where(date: Time.now.beginning_of_month..Time.now.end_of_month).sum(:amount)
     @proportion = total_expense / total_budget.to_f
+    @monthly_income = current_user.profile.monthly_income
+
+
+
+    days_total_expense = Expense.where(date: Time.now.beginning_of_week..Time.now.end_of_week).sum(:amount)
+    days_income = @monthly_income / 4
+
+    @days_chart_data = [
+      ['Expenses', days_total_expense],
+      ['Income', days_income]
+    ]
+
+    month_income = @monthly_income
+    month_total_expense = Expense.where(date: Time.now.beginning_of_month..Time.now.end_of_month).sum(:amount)
+
+    @month_chart_data = [
+      ['Expenses', month_total_expense],
+      ['Income', month_income]
+    ]
+
+    year_income = @monthly_income * 12
+    year_total_expenses = Expense.where(date: Time.now.beginning_of_year..Time.now.end_of_year).sum(:amount)
+
+    @year_chart_data = [
+      ['Expenses', year_total_expenses],
+      ['Income', year_income]
+    ]
 
     if @proportion > 1
       @message = Tip.where(advice_level: 'over_100').sample.content
